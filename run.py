a5ffa34fe59aaddcdd704688097420cc3eee4eed
Majdi-Aref <>
2023-06-23 21:55:53 +0000

    feat: add a function that enables a user to enter their desired password length

--- run.py
+++ run.py
@@ -6,3 +6,14 @@ import random
 import string
 
 
+def get_password_minimum_length():
+    """Gets a user's desired minimum number of characters of their password"""
+    while True:
+        minimum_length_user_input = input("Enter a number that represents the minimum length of the password that you want; the number must be greater than 5: ")
+        if minimum_length_user_input.isdigit() == False:
+            continue
+        desired_minimum_length = int(minimum_length_user_input)
+        if desired_minimum_length < 6:
+            continue
+        return desired_minimum_length
+
