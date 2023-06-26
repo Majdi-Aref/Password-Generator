f91c1d7703f77ff3106febab20c23aa1114af383
Majdi-Aref <>
2023-06-26 23:05:44 +0000

    style: make code lines' lengths of the main function in agreement with the CI Python Linter's guidelines

--- run.py
+++ run.py
@@ -98,8 +98,13 @@ def main():
     has_numbers = does_user_want_numbers()
     has_special_chars = does_user_want_special_characters()
     characters_source = get_characters_source(has_numbers, has_special_chars)
-    password = generate_password(minimum_length, characters_source, has_numbers, has_special_chars)
-    print(Fore.GREEN + "Your requested password is:\n" + Fore.YELLOW + password)
+    password = (
+        generate_password(minimum_length, characters_source,
+                          has_numbers, has_special_chars)
+    )
+    print(
+        (Fore.GREEN + "Your requested password is:\n" + Fore.YELLOW + password)
+    )
 
 
 main()
